06f74ae5f0d68b0ddcc30dd58595bd9924ddd199
Jackson Rushing <j@jacksonrushing.com>
2016-09-15 21:20:51 -0400

    Matrix operations
    Addition, subtraction, output overloaded.

---
+++ src/MMatrix.cpp
@@ -0,0 +1,119 @@
+#include "MMatrix.h"
+// Y before X, to avoid flipping variable names everywhere else and preventing crashes and general confusion. What we want, (X,Y) is the opposite
+//of how C++ stores stuff.
+MMatrix::MMatrix(int y, int x)
+{
+	sizeX = y;
+	sizeY = x;
+	matrixData = new float* [y];
+
+	for (int i = 0; i < y; i++)
+	{
+		matrixData[i] = new float [x];
+	}
+
+	for (int i = 0; i < y; i++)
+	{
+		for (int j = 0; j < x; j++)
+		{
+			matrixData[i][j] = 0.0f;
+		}
+	}
+}
+
+MMatrix::~MMatrix()
+{
+	//sadlkfjlkasdfkjlsalsfdajjkkjsdfjkdla
+}
+//Needs try/catch
+void MMatrix::inputValue(int x, int y, float val)
+{
+	this->matrixData[x][y] = val;
+}
+//Needs try/catch
+float MMatrix::getValue(int x, int y) const
+{
+	return this->matrixData[x][y];
+}
+
+
+MMatrix MMatrix::operator+(MMatrix operand)
+{
+	try
+	{
+		if ((this->getsizeX() == operand.getsizeX()) && (this->getsizeY() == operand.getsizeY()))
+		{
+			for (int i = 0; i <	sizeX; i++)
+			{
+				for (int j = 0; j < sizeY; j++)
+				{
+					inputValue(i, j, getValue(i, j) + operand.getValue(i, j));
+				}
+			}
+			return *this;
+		}
+		else
+		{
+			throw(0);
+		}
+	}
+	catch(...)
+	{
+		std::cout << "Aborting! Added matrices are not the same dimensions!!!" << std::endl;
+		abort();
+	}
+	
+}
+
+MMatrix MMatrix::operator-(MMatrix operand)
+{
+	try
+	{
+		if ((this->getsizeX() == operand.getsizeX()) && (this->getsizeY() == operand.getsizeY()))
+		{
+			for (int i = 0; i < sizeX; i++)
+			{
+				for (int j = 0; j < sizeY; j++)
+				{
+					inputValue(i, j, getValue(i, j) - operand.getValue(i, j));
+				}
+			}
+			return *this;
+		}
+		else
+		{
+			throw(0);
+		}
+	}
+	catch (...)
+	{
+		std::cout << "Aborting! Added matrices are not the same dimensions!!!" << std::endl;
+		abort();
+	}
+
+}
+
+int MMatrix::getsizeX() const
+{
+	return sizeX;
+}
+
+int MMatrix::getsizeY() const
+{
+	return sizeY;
+}
+std::ostream& operator<< (std::ostream& os, const MMatrix mat)
+{
+
+	for (int i = 0; i < mat.getsizeY(); i++)
+	{
+		os << std::endl;
+		for (int j = 0; j < mat.getsizeX(); j++)
+		{
+			os << "[" << mat.getValue(j, i) << "]";
+		}
+	}
+
+	return os;
+}
+
--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,29 @@
 
 void main()
 {
+	MMatrix matrix(2, 3);
+	MMatrix matrix2(2, 3);
+	MMatrix matrix3(2, 3);
 
+	matrix.inputValue(0, 0, 1);
+	matrix.inputValue(1, 0, 2);
+	matrix.inputValue(0, 1, 3);
+	matrix.inputValue(1, 1, 4);
+	matrix.inputValue(0, 2, 5);
+	matrix.inputValue(1, 2, 6);
+
+	matrix2.inputValue(0, 0, 3);
+	matrix2.inputValue(1, 0, 2);
+	matrix2.inputValue(0, 1, 5);
+	matrix2.inputValue(1, 1, 6);
+	matrix2.inputValue(0, 2, 9);
+	matrix2.inputValue(1, 2, 7);
+
+	matrix3.inputValue(0, 0, 5);
+	matrix3.inputValue(1, 0, -4);
+	matrix3.inputValue(0, 1, 7);
+
+	std::cout << matrix - matrix2 + matrix3;
+	
+	system("pause");
 }
